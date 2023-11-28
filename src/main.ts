bcc4248742ddd5f4e63781ac5894db84be425531
Ameon <ameon@ya.ru>
2023-11-28 13:53:12 +0500

    Added output info about the running server to console.log

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { redBright, blueBright, yellowBright } from 'console-log-colors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const PORT = process.env.Port || 3001;
+  const HOST = process.env.HOST || 'localhost';
+
+  await app.listen(PORT, () => {
+    console.log(
+      blueBright(`Сервер запущен по адресу: `) +
+        yellowBright(`http://${HOST}:`) +
+        redBright(`${PORT}`),
+    );
+  });
 }
 bootstrap();
