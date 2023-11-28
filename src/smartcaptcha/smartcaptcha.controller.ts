2ca59626b3b8b1fc99ab2670fb3842ffd83d2379
Ameon <ameon@ya.ru>
2023-11-28 14:55:41 +0500

    Added a new check route to smartcaptcha.controller.ts

--- src/smartcaptcha/smartcaptcha.controller.ts
+++ src/smartcaptcha/smartcaptcha.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { SmartcaptchaService } from './smartcaptcha.service';
 
 @Controller('smartcaptcha')
 export class SmartcaptchaController {
   constructor(private readonly smartcaptchaService: SmartcaptchaService) {}
+
+  @Post('check')
+  async checkCapcha(@Body('token') token: string) {
+    return await this.smartcaptchaService.checkCaptcha(token);
+  }
 }
