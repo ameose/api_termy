a32198904d3f12ebe24f40d827aa8df8672c1047
Ameon <ameon@ya.ru>
2023-11-28 14:42:48 +0500

    Connected http module to smartcaptcha.module.ts

--- src/smartcaptcha/smartcaptcha.module.ts
+++ src/smartcaptcha/smartcaptcha.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SmartcaptchaService } from './smartcaptcha.service';
 import { SmartcaptchaController } from './smartcaptcha.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   controllers: [SmartcaptchaController],
+  imports: [HttpModule],
   providers: [SmartcaptchaService],
 })
 export class SmartcaptchaModule {}
