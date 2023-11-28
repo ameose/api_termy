815675fb55b9d601d4c2b1f4588994a45698ee27
Ameon <ameon@ya.ru>
2023-11-28 15:26:48 +0500

    Created a service smartcaptcha.service.ts for checking captcha

--- src/smartcaptcha/smartcaptcha.service.ts
+++ src/smartcaptcha/smartcaptcha.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { map } from 'rxjs/operators';
+import { HttpService } from '@nestjs/axios';
 
 @Injectable()
-export class SmartcaptchaService {}
+export class SmartcaptchaService {
+  constructor(private readonly httpService: HttpService) {}
+
+  async checkCaptcha(token: string): Promise<boolean> {
+    const url = 'https://smartcaptcha.yandexcloud.net/validate';
+    const params = {
+      secret: '',
+      token,
+    };
+
+    const response$ = this.httpService.get(url, { params });
+
+    return response$
+      .pipe(map((response) => response.data.status === 'ok'))
+      .toPromise();
+  }
+}
