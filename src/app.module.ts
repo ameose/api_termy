7e98f3dc83495be446e2000613716617371a3cd8
Ameon <ameon@ya.ru>
2023-11-28 15:38:56 +0500

    Registering ConfigModule as a global module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SmartcaptchaModule } from './smartcaptcha/smartcaptcha.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [SmartcaptchaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Сделаем конфигурацию глобальной для всего приложения
+    }),
+    SmartcaptchaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
