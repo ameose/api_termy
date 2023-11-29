ed309074bb0ddcad10b1a1570e714b5afef56a9a
Ameon <ameon@ya.ru>
2023-11-29 18:24:10 +0500

    Fixed ESLint

--- src/anketa/anketa.controller.ts
+++ src/anketa/anketa.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { AnketaService } from './anketa.service';
 import { CreateAnketaDto } from './dto/create-anketa.dto';
 import { UpdateAnketaDto } from './dto/update-anketa.dto';
