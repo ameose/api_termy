353022267b0a8a35e044eb44f86c4974169852fa
Ameon <ameon@ya.ru>
2023-11-29 19:50:57 +0500

    Removed unused DTOs

--- src/anketa/anketa.service.ts
+++ src/anketa/anketa.service.ts
@@ -1,26 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAnketaDto } from './dto/create-anketa.dto';
-import { UpdateAnketaDto } from './dto/update-anketa.dto';
 
 @Injectable()
-export class AnketaService {
-  create(createAnketaDto: CreateAnketaDto) {
-    return 'This action adds a new anketa';
-  }
-
-  findAll() {
-    return `This action returns all anketa`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} anketa`;
-  }
-
-  update(id: number, updateAnketaDto: UpdateAnketaDto) {
-    return `This action updates a #${id} anketa`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} anketa`;
-  }
-}
+export class AnketaService {}
--- src/anketa/dto/update-anketa.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateAnketaDto } from './create-anketa.dto';
-
-export class UpdateAnketaDto extends PartialType(CreateAnketaDto) {}
