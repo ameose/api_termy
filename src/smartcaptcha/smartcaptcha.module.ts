5091f1f57472e11099d2fb16c91c099ac3d3c67a
Ameon <ameon@ya.ru>
2023-11-29 19:59:27 +0500

    Connected PhoneController PhoneService in smartcaptcha.module.ts

--- src/smartcaptcha/smartcaptcha.module.ts
+++ src/smartcaptcha/smartcaptcha.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { SmartcaptchaService } from './smartcaptcha.service';
 import { SmartcaptchaController } from './smartcaptcha.controller';
 import { HttpModule } from '@nestjs/axios';
+import { PhoneService } from '../phone/phone.service';
+import { PhoneController } from '../phone/phone.controller';
 
 @Module({
-  controllers: [SmartcaptchaController],
+  controllers: [SmartcaptchaController, PhoneController],
   imports: [HttpModule],
-  providers: [SmartcaptchaService],
+  providers: [SmartcaptchaService, PhoneService],
 })
 export class SmartcaptchaModule {}
