1239c8a28c445ce54dffeabf54db6bf0502708af
Ameon <ameon@ya.ru>
2023-11-29 18:25:44 +0500

    Added a method called generateCode to phone.service.ts

--- src/phone/phone.service.ts
+++ src/phone/phone.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class PhoneService {}
+export class PhoneService {
+  private generatedCode: string;
+
+  private generateCode(): void {
+    this.generatedCode = Math.floor(
+      Math.random() * (9999 - 1000 + 1) + 1000,
+    ).toString();
+  }
+}
