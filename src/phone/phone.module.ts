b32100a2b8709ca41c0514612b2cbeaaf238bd07
Ameon <ameon@ya.ru>
2023-11-29 16:19:05 +0500

    Connected http module to smartcaptcha.module.ts

--- src/phone/phone.module.ts
+++ src/phone/phone.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PhoneService } from './phone.service';
 import { PhoneController } from './phone.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   controllers: [PhoneController],
+  imports: [HttpModule],
   providers: [PhoneService],
 })
 export class PhoneModule {}
