9347d079c922eacb4910985723df101d1843df3a
Ameon <ameon@ya.ru>
2023-11-29 13:00:00 +0500

    Added the token parameter to swagger in smartcaptcha.controller.ts

--- src/smartcaptcha/smartcaptcha.controller.ts
+++ src/smartcaptcha/smartcaptcha.controller.ts
@@ -1,11 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { SmartcaptchaService } from './smartcaptcha.service';
+import { ApiBody } from '@nestjs/swagger';
 
 @Controller('smartcaptcha')
 export class SmartcaptchaController {
   constructor(private readonly smartcaptchaService: SmartcaptchaService) {}
 
   @Post('check')
+  @ApiBody({
+    schema: { type: 'object', properties: { token: { type: 'string' } } },
+  })
   async checkCapcha(@Body('token') token: string) {
     return await this.smartcaptchaService.checkCaptcha(token);
   }
