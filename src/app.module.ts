1963b0abd9c35de16888d2a67cced62fd45dfc09
Ameon <ameon@ya.ru>
2023-11-29 15:55:46 +0500

    Generated resourse for phone

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { SmartcaptchaModule } from './smartcaptcha/smartcaptcha.module';
 import { ConfigModule } from '@nestjs/config';
 import { AnketaModule } from './anketa/anketa.module';
+import { PhoneModule } from './phone/phone.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { AnketaModule } from './anketa/anketa.module';
     }),
     SmartcaptchaModule,
     AnketaModule,
+    PhoneModule,
   ],
   controllers: [AppController],
   providers: [AppService],
