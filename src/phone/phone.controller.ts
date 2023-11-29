440b9ab81c132ceadfd9f7851e15e8759a6f6486
Ameon <ameon@ya.ru>
2023-11-29 17:53:23 +0500

    Added getcode route and description for swagger to phone.controller.ts

--- src/phone/phone.controller.ts
+++ src/phone/phone.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { PhoneService } from './phone.service';
+import { ApiOperation, ApiResponse, ApiTags, ApiBody } from '@nestjs/swagger';
 
+@ApiTags('phone')
 @Controller('phone')
 export class PhoneController {
   constructor(private readonly phoneService: PhoneService) {}
+
+  @Post('getcode')
+  @ApiOperation({ summary: 'Получить код' })
+  @ApiResponse({ status: 200, description: 'Код успешно отправлен' })
+  @ApiResponse({ status: 400, description: 'Неверный запрос' })
+  @ApiBody({ description: 'Номер телефона', type: String })
+  async getCode(@Body('phone') phone: string): Promise<void> {
+    const context = { phone: phone };
+    // this.phoneService.setContext(context);
+    // console.log(this.phoneService.getContext());
+    await this.phoneService.getCode(context.phone);
+  }
 }
