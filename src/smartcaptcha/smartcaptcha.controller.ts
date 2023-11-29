a296b64d81e26cd63365ef0b60f0d6a683e6f05f
Ameon <ameon@ya.ru>
2023-11-29 20:40:06 +0500

    Improved checkCapcha method and description for swagger in smartcaptcha.controller.ts

--- src/smartcaptcha/smartcaptcha.controller.ts
+++ src/smartcaptcha/smartcaptcha.controller.ts
@@ -1,16 +1,47 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { SmartcaptchaService } from './smartcaptcha.service';
-import { ApiBody } from '@nestjs/swagger';
+import { ApiBody, ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { PhoneService } from '../phone/phone.service';
 
+@ApiTags('smartcaptcha')
 @Controller('smartcaptcha')
 export class SmartcaptchaController {
-  constructor(private readonly smartcaptchaService: SmartcaptchaService) {}
+  constructor(
+    private readonly smartcaptchaService: SmartcaptchaService,
+    private readonly phoneService: PhoneService,
+  ) {}
 
   @Post('check')
-  @ApiBody({
-    schema: { type: 'object', properties: { token: { type: 'string' } } },
+  @ApiOperation({
+    summary: 'Проверка капчи и отправка кода на телефон',
+    description:
+      'Проверяет капчу по предоставленному токену. Если капча успешно проверена, отправляет код на указанный номер телефона.',
   })
-  async checkCapcha(@Body('token') token: string) {
+  @ApiBody({
+    description: 'Проверка капчи и отправка кода на телефон',
+    schema: {
+      type: 'object',
+      properties: {
+        token: { type: 'string', description: 'Токен для проверки капчи' },
+        phone: {
+          type: 'string',
+          description: 'Номер телефона для отправки кода',
+        },
+      },
+    },
+  })
+  @ApiResponse({ status: 200, description: 'Код успешно отправлен' })
+  @ApiResponse({
+    status: 400,
+    description: 'Неверный токен или ошибка при отправке кода',
+  })
+  async checkCaptcha(
+    @Body('token') token: string,
+    @Body('phone') phone: string,
+  ) {
+    if (await this.smartcaptchaService.checkCaptcha(token)) {
+      return await this.phoneService.sendCode(phone);
+    }
     return await this.smartcaptchaService.checkCaptcha(token);
   }
 }
