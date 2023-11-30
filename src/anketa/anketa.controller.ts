3054cfc33c7978d37d72e6e1d8040abe5d357806
Ameon <ameon@ya.ru>
2023-11-30 21:21:36 +0500

    Added description for swagger to anketa.controller.ts

--- src/anketa/anketa.controller.ts
+++ src/anketa/anketa.controller.ts
@@ -1,8 +1,26 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpException,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 import { AnketaService } from './anketa.service';
 import { PhoneService } from '../phone/phone.service';
 import { HttpService } from '@nestjs/axios';
 import { ConfigService } from '@nestjs/config';
+import {
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+  ApiBody,
+  ApiBadRequestResponse,
+  ApiOkResponse,
+  ApiNotFoundResponse,
+} from '@nestjs/swagger';
+import { SendAnketaDto } from './dto/send-anketa.dto';
+import { GetAnketaDto } from './dto/get-anketa.dto';
+import { AnketaResponseDto } from './dto/anketa-response.dto';
 
 interface Child {
   id: string;
@@ -10,6 +28,7 @@ interface Child {
   birthday: string;
 }
 
+@ApiTags('anketa')
 @Controller('anketa')
 export class AnketaController {
   constructor(
@@ -19,6 +38,10 @@ export class AnketaController {
     private readonly configService: ConfigService,
   ) {}
 
+  @ApiOperation({ summary: 'Отправка анкеты' })
+  @ApiResponse({ status: 200, description: 'Анкета успешно отправлена' })
+  @ApiBadRequestResponse({ description: 'Неверный запрос' })
+  @ApiBody({ type: SendAnketaDto })
   @Post('sendAnketa')
   async sendAnketa(
     @Body('number') number: number,
@@ -40,14 +63,28 @@ export class AnketaController {
     );
   }
 
+  @ApiOperation({ summary: 'Получение анкеты' })
+  @ApiOkResponse({
+    status: 200,
+    description: 'Анкета успешно получена',
+    type: AnketaResponseDto,
+  })
+  @ApiBadRequestResponse({ description: 'Неверный запрос' })
+  @ApiNotFoundResponse({ description: 'Анкета не найдена' })
+  @ApiBody({ type: GetAnketaDto })
   @Post('getAnketa')
-  async getAnketa(@Body('phone') phone: string) {
-    const url = `${this.configService.get('API_URL')}/Anketa/${phone
-      .replace('+7', '')
-      .replace('(', '')
-      .replace(')', '')
-      .replace('-', '')}`;
-    const response = await this.http.get(url);
-    return response;
+  async getAnketa(@Body() getAnketaDto: GetAnketaDto) {
+    // Очистка номера телефона от нецифровых символов
+    const phone = getAnketaDto.phone.replace(/\D/g, '');
+
+    // Формирование URL с использованием очищенного номера телефона
+    const url = `${this.configService.get('API_URL')}/Anketa/${phone}`;
+
+    try {
+      const response = await this.http.get(url).toPromise();
+      return response.data; // Предполагая, что данные соответствуют AnketaResponseDto
+    } catch (error) {
+      throw new HttpException('Анкета не найдена', HttpStatus.NOT_FOUND);
+    }
   }
 }
