d5e45518b9c72687dc447af96faf92d785baaf8c
Ameon <ameon@ya.ru>
2023-11-30 21:17:23 +0500

    Added a list of allowed domains http://termy.honc.ru/ and https://termy.honc.ru/ to cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'http://termy.honc.ru/',
+      'https://termy.honc.ru/',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept',
   });
