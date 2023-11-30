61f964f11c1e71a10cad26b1b209fc87d6d75b0d
Ameon <ameon@ya.ru>
2023-11-30 17:29:35 +0500

    Added to HttpModule imports, and to PhoneService providers

--- src/anketa/anketa.module.ts
+++ src/anketa/anketa.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AnketaService } from './anketa.service';
 import { AnketaController } from './anketa.controller';
+import { HttpModule } from '@nestjs/axios';
+import { PhoneService } from '../phone/phone.service';
 
 @Module({
   controllers: [AnketaController],
-  providers: [AnketaService],
+  imports: [HttpModule],
+  providers: [AnketaService, PhoneService],
 })
 export class AnketaModule {}
