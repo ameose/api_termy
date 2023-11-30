86ca34acf0b2a1bbce36c6a312e420b1daca888f
Ameon <ameon@ya.ru>
2023-11-30 11:52:51 +0500

    Added a new route called verify-phone to smartcaptcha.controller.ts

--- src/smartcaptcha/smartcaptcha.controller.ts
+++ src/smartcaptcha/smartcaptcha.controller.ts
@@ -44,4 +44,33 @@ export class SmartcaptchaController {
     }
     return await this.smartcaptchaService.checkCaptcha(token);
   }
+
+  @Post('verify-phone')
+  @ApiOperation({
+    summary: 'Проверка кода, отправленного на телефон',
+    description:
+      'Сравнивает предоставленный код с кодом, сгенерированным на сервере, для верификации номера телефона.',
+  })
+  @ApiBody({
+    description: 'Тело запроса для верификации телефона',
+    schema: {
+      type: 'object',
+      properties: {
+        code: {
+          type: 'string',
+          description: 'Код для верификации, отправленный на телефон',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Верификация успешна (true) или неуспешна (false)',
+  })
+  @ApiResponse({ status: 400, description: 'Неверный запрос, отсутствует код' })
+  async verifyPhone(@Body('code') code: string): Promise<boolean> {
+    const generatedCode = await this.phoneService.getGeneratedCode();
+    console.log(`${generatedCode} ${code}`);
+    return code === generatedCode;
+  }
 }
