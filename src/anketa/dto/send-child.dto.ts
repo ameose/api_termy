6ed975e1ef3079f6a5f97d5ea1e4fac12b55546e
Ameon <ameon@ya.ru>
2023-12-05 14:48:15 +0500

    Renamed SendChildDto to CreateChildDto

--- src/anketa/dto/send-child.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class SendChildDto {
-  @ApiProperty({ description: 'Имя ребенка' })
-  name: string;
-
-  @ApiProperty({ description: 'Дата рождения ребенка' })
-  birthday: string;
-}
